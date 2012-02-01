393f212104ed29c761c2479651e215fc67d15338
Christophe <christophe.belpaire@gmail.com>
2012-02-01 11:31:36 +0100

    fix twitter initializer

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,7 +1,5 @@
-if File.exists? "#{Rails.root}/config/facebook.yml"
-
+if File.exists? "#{Rails.root}/config/twitter.yml"
   twitter_config = YAML::load(File.open("#{Rails.root}/config/twitter.yml"))[Rails.env]
-
   Twitter.configure do |config|
     config.consumer_key = twitter_config['consumer_key']
     config.consumer_secret = twitter_config['consumer_secret']
